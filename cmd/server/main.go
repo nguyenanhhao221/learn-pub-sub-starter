6e054970f3a452bc4d7e8934fd6b4049b273f273
Hao Nguyen <hao@haonguyen.tech>
2025-01-12 02:10:42 +0700

    feat: init rabitmq connection and shutdown message

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	CONN_STRING := "amqp://guest:guest@localhost:5672/"
 	fmt.Println("Starting Peril server...")
+	conn, err := amqp.Dial(CONN_STRING)
+	if err != nil {
+		log.Fatalf("Error connection to rabittmq: %v", err)
+	}
+	defer conn.Close()
+	log.Println("Connect to rabittmq successfully!")
+
+	// Capture exit signal and print exit message
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt)
+	for range signalCh {
+		log.Println("Shutting Down!")
+		os.Exit(1)
+		return
+	}
 }
