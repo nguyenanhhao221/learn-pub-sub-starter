3ea73976b4632096b6528494df22ee0b53f4650f
Hao Nguyen <hao@haonguyen.tech>
2025-01-12 19:59:02 +0700

    chore: add temporary command to run rabbitmq with stomp

--- rabbit.sh
+++ rabbit.sh
@@ -4,6 +4,7 @@ case "$1" in
     start)
         echo "Starting RabbitMQ container..."
         docker run -d --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management
+        # docker run -d --rm --name rabbitmq-stomp -p 61613:61613 -p 15672:15672 rabbitmq-stomp
         ;;
     stop)
         echo "Stopping RabbitMQ container..."
