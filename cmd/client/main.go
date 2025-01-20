24b7bf4857ff0afd86a018874ab10dca2f0d2ea3
Hao Nguyen <hao@haonguyen.tech>
2025-01-20 19:03:24 +0700

    feat(client): create queue and bind them

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/nguyenanhhao221/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/nguyenanhhao221/learn-pub-sub-starter/internal/pubsub"
+	"github.com/nguyenanhhao221/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	CONN_STRING := "amqp://guest:guest@localhost:5672/"
 	fmt.Println("Starting Peril client...")
+
+	conn, err := amqp.Dial(CONN_STRING)
+	if err != nil {
+		log.Fatalf("Error connection to rabittmq: %v", err)
+	}
+	defer conn.Close()
+	log.Println("Connect to rabittmq successfully!")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("Cannot get username: %v", err)
+	}
+	exchangeName := fmt.Sprintf("%s.%s", routing.PauseKey, username)
+	log.Printf("Using exchange name: %q", exchangeName)
+	_, queue, err := pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, exchangeName, routing.PauseKey, 1)
+	if err != nil {
+		log.Fatalf("could not subscribe to pause: %v", err)
+	}
+	fmt.Printf("Queue %v declared and bound!\n", queue.Name)
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -3,9 +3,55 @@ package pubsub
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
+
+type SimpleQueueType int
+
+const (
+	SimpleQueueDurable SimpleQueueType = iota
+	SimpleQueueTransient
+)
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	simpleQueueType SimpleQueueType, // an enum to represent "durable" or "transient"
+) (*amqp.Channel, amqp.Queue, error) {
+	// Create a channel to start the process
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+	queue, err := ch.QueueDeclare(
+		queueName,                             // name
+		simpleQueueType == SimpleQueueDurable, // durable
+		simpleQueueType != SimpleQueueDurable, // delete when unused
+		simpleQueueType != SimpleQueueDurable, // exclusive
+		false,                                 // no-wait
+		nil,                                   // arguments
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("could not declare queue: %v", err)
+	}
+
+	// Bind the queue to an exchange
+	err = ch.QueueBind(
+		queue.Name, // queue name
+		key,        // routing key
+		exchange,   // exchange
+		false,      // no-wait
+		nil,        // args
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("could not bind queue: %v", err)
+	}
+	return ch, queue, nil
+}
 
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	ctx := context.Background()
