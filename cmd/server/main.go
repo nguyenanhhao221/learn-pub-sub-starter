fa30b97713f000d8c4caf1eb9dd63e8519074419
Hao Nguyen <hao@haonguyen.tech>
2025-01-21 20:56:35 +0700

    feat(server): add relp and temporary remove quit signal

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
+	"github.com/nguyenanhhao221/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/nguyenanhhao221/learn-pub-sub-starter/internal/pubsub"
 	"github.com/nguyenanhhao221/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -22,20 +21,45 @@ func main() {
 	log.Println("Connect to rabittmq successfully!")
 
 	// Capture exit signal and print exit message
-	signalCh := make(chan os.Signal, 1)
-	signal.Notify(signalCh, os.Interrupt)
-	for range signalCh {
-		log.Println("Shutting Down!")
-		os.Exit(1)
-		return
-	}
+	// signalCh := make(chan os.Signal, 1)
+	// signal.Notify(signalCh, os.Interrupt)
+	// for range signalCh {
+	// 	log.Println("Shutting Down!")
+	// 	os.Exit(1)
+	// 	return
+	// }
 
-	amqpCh, err := conn.Channel()
+	publishCh, err := conn.Channel()
 	if err != nil {
 		log.Println("Error opening channel", err)
 	}
 
-	if err := pubsub.PublishJSON(amqpCh, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true}); err != nil {
-		log.Printf("Error PublishJSON %v", err)
+	gamelogic.PrintServerHelp()
+	for {
+		input := gamelogic.GetInput()
+		if len(input) <= 0 {
+			continue
+		}
+		switch input[0] {
+		case "pause":
+			log.Println("Pausing the game")
+			// Publish the pausing message to the broker
+			if err := pubsub.PublishJSON(publishCh, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true}); err != nil {
+				log.Printf("Error PublishJSON %v", err)
+			}
+			log.Println("Pause message published successfully")
+		case "resume":
+			log.Println("Resuming the game")
+			if err := pubsub.PublishJSON(publishCh, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: false}); err != nil {
+				log.Printf("Error PublishJSON %v", err)
+			}
+			log.Println("Resume game successfully")
+		case "quit":
+			log.Println("Quitting game")
+			return
+		default:
+			log.Println("Don't understand the command")
+			continue
+		}
 	}
 }
